4966412f942ce3091198f1d899cf403a588d9707
Aklauser1 <31425004+Aklauser1@users.noreply.github.com>
2018-01-31 20:22:58 -0600

    initial commit

---
+++ Interactive Maryville Map/Interactive Maryville Map/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Interactive Maryville Map
+//
+//  Created by Brandon Schmidt on 1/30/18.
+//  Copyright © 2018 Drew Klauser. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let location = CLLocation(latitude: 38.645247, longitude: -90.504252)
+        let region = MKCoordinateRegionMakeWithDistance(location.coordinate, 600, 600)
+        mapView.setRegion(region, animated: true)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
